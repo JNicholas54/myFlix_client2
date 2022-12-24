d30768b0e5c5d0ec6ecff4ce273e080db583cc2a
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-12-23 20:11:09 -0800

    built app, updated readme, add dist folder

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+
+//import statement to indicate that you need to bundle './index.scss'
+import "./index.scss"
+
+//Main component will eventually use all the others
+const MyFlixApplication = () => {
+    return (
+        <div className="my-flix">
+            <div>Good morning!</div>
+        </div>
+    );
+};
+
+//Finds the root of your app
+const container = document.querySelector("#root");
+const root = createRoot(container);
