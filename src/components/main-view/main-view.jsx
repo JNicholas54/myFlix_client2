77efbcabd9f8eb0d23c239be14188ad8f929fa75
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-12-26 17:20:43 -0800

    including proptypes, adding useEffect to fetch.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,52 +1,34 @@
-import { useState } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
 
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Good Will Hunting",
-            image:
-            "https://m.media-amazon.com/images/M/MV5BOTI0MzcxMTYtZDVkMy00NjY1LTgyMTYtZmUxN2M3NmQ2NWJhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX67_CR0,0,67,98_AL_.jpg",
-            director: "Gus Van Sant"
-        },
-        {
-            id: 2,
-            title: "A Few Good Men",
-            image:
-            "https://m.media-amazon.com/images/M/MV5BMmRlZDQ1MmUtMzE2Yi00YTkxLTk1MGMtYmIyYWQwODcxYzRlXkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_UX67_CR0,0,67,98_AL_.jpg",
-            director: "Rob Reiner"
-        },
-        {
-            id: 3,
-            title: "My Cousin Vinny",
-            image:
-            "https://m.media-amazon.com/images/M/MV5BMTQxNDYzMTg1M15BMl5BanBnXkFtZTgwNzk4MDgxMTE@._V1_UX67_CR0,0,67,98_AL_.jpg",
-            director: "Johnathan Lynn"
-        },
-        {
-            id: 4,
-            title: "A Nightmare on Elm Street",
-            image:
-            "https://m.media-amazon.com/images/M/MV5BNzFjZmM1ODgtMDBkMS00NWFlLTg2YmUtZjc3ZTgxMjE1OTI2L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX67_CR0,0,67,98_AL_.jpg",
-            director: "Wes Craven"
-        },
-        {
-            id: 5,
-            title: "Avatar",
-            image:
-            "https://m.media-amazon.com/images/M/MV5BNjA3NGExZDktNDlhZC00NjYyLTgwNmUtZWUzMDYwMTZjZWUyXkEyXkFqcGdeQXVyMTU1MDM3NDk0._V1_UX67_CR0,0,67,98_AL_.jpg",
-            director: "James Cameron"
-        }
-    ]);
-    
-    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("https://guarded-wave-99547.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then((data) => {
+            const movies = data.docs.map((doc) => {
+                return {
+                    id: doc.key,
+                    title: doc.title,
+                    director: doc.director_name?.[0]
+                };
+            });
+        
+        setMovies(movies);
+    });
+}, []);
+
+const [selectedMovie, setSelectedMovie] = useState(null);
+
 
     if (selectedMovie) {
-        return (<MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        return ( <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
         );
     }
 
@@ -66,4 +48,4 @@ export const MainView = () => {
                 ))}
             </div>
         );
-    }
+    };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+//This is the moviecard function component
 export const MovieCard = ({movie, onMovieClick }) => {
     return <div onClick={() => { onMovieClick(movie); }}>{movie.title}</div>;
+  };
+
+  //Here is where we define all the props constraints for the moviecard
+  MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      story: PropTypes.string.isRequired,
+      genre: PropTypes.shape({
+        name: PropTypes.string,
+        description: PropTypes.string
+      }),
+      director: PropTypes.shape({
+        name: PropTypes.string,
+        bio: PropTypes.string,
+        dob: PropTypes.string
+      }),
+      ImagePath: PropTypes.string,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
   };
